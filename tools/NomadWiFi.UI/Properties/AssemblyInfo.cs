7d79ab543be54545738dfb1c683a567b60d44bc0
Dario Mory <dario@mory.dev>
2026-09-05 07:30:51 +0700

    chore: stamp version resources for 1.3.1

--- tools/NomadWiFi.UI/Properties/AssemblyInfo.cs
+++ tools/NomadWiFi.UI/Properties/AssemblyInfo.cs
@@ -11,9 +11,9 @@ using System.Windows;
 [assembly: AssemblyCompany("mory.dev")]
 [assembly: AssemblyProduct("NomadWiFi")]
 [assembly: AssemblyCopyright("MIT Licensed")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
-[assembly: AssemblyInformationalVersion("1.3.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyInformationalVersion("1.3.1")]
 
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en")]
